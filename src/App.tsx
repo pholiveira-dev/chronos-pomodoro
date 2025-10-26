faedbd3fa01edcc9546c4dd20b70979bea5a471b
Pedro <pedro.alves@escs.edu.br>
2025-10-25 23:08:42 -0300

    feat: Estrutura inicial dos componentes e padronização de arquivos" -m "Introduz a estrutura básica para componentes reutilizáveis. * Cria o padrão de pastas (Componente/Container/Heading). * Padroniza arquivos index.tsx e styles.modules.css. * Atualiza App.tsx e main.tsx para integrar os novos componentes. * Inicia as configurações nos arquivos global.css e theme.css (WIP).

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles/theme.css";
+import "./styles/global.css";
+import { Container } from "./components/Container";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Heading } from "./components/Heading";
 
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Container>
+        <Heading>Chronos</Heading>
+      </Container>
 
-export default App
+      <Container>
+        <section>MENU</section>
+      </Container>
+
+      <Container>
+        <section>FORM</section>
+      </Container>
+
+      <Container>
+        <section>FOOTER</section>
+      </Container>
+    </>
+  );
+}
