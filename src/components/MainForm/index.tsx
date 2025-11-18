16f2639d08d74050fdb0a770f6c75f3e63f603bd
Pedro <pedro.alves@escs.edu.br>
2025-11-18 17:05:45 -0300

    refactor: CountDown, MainForm e Home

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,13 +2,31 @@ import { DefaultInput } from "../DefaultInput";
 import { DefaultButton } from "../DefaultButton";
 import { Cycles } from "../Cycles";
 import { PlayCircleIcon } from "lucide-react";
+import { type HomeProps } from "../../pages/Home";
 
-export function MainForm() {
+export function MainForm({ state, setState }: HomeProps) {
+  function handleClick() {
+    setState((prevState) => {
+      return {
+        ...prevState,
+        config: {
+          ...prevState.config,
+          workTime: 34,
+        },
+        formattedSecondsRemaining: "23:34",
+      };
+    });
+  }
   return (
     <form className="form" action="">
+      <div>
+        <button type="button" onClick={handleClick}>
+          Clicar
+        </button>
+      </div>
       <div className="formRow">
         <DefaultInput
-          labelText="tesk"
+          labelText="task"
           id="meuInput"
           type="text"
           placeholder="Ex.: estudar para a prova"
@@ -16,7 +34,7 @@ export function MainForm() {
       </div>
 
       <div className="formRow">
-        <p>Lorem ipsum dolor sit amet.</p>
+        <p>O próximo intervalo é de {state.config.workTime} min</p>
       </div>
 
       <div className="formRow"></div>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,16 +2,22 @@ import { MainTemplate } from "../../templates/MainTemplate";
 import { Container } from "../../components/Container";
 import { CountDown } from "../../components/CountDown";
 import { MainForm } from "../../components/MainForm";
+import type { TaskStateModel } from "../../models/TaskStateModel";
 
-export function Home() {
+export type HomeProps = {
+  state: TaskStateModel;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
+
+export function Home(props: HomeProps) {
   return (
     <MainTemplate>
       <Container>
-        <CountDown />
+        <CountDown {...props} />
       </Container>
 
       <Container>
-        <MainForm />
+        <MainForm {...props} />
       </Container>
     </MainTemplate>
   );
